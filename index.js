b5dcacc701528b824c95fac97554bf9612f8a640
DarksideEvil <sinister58450@gmail.com>
2024-01-04 17:00:36 +0500

    intial commit 5

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ console.log('hello world !');
 console.log('hell9ok');
 console.log('hello world !');
 console.log('hello world !');
-console.log('hello world !');
+console.log('hello world ozirgisi');
